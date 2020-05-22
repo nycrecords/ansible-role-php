d86cb82c74f0df07910f89389c8a1e360e33398c
Gary Zhou <gzhou@records.nyc.gov>
2020-05-22 16:24:05 -0400

    Parametrize test

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,8 +7,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ["MOLECULE_INVENTORY_FILE"]
 ).get_hosts("all")
 
-
-def test_php_installed(host):
-    php = host.package("rh-php72")
-
-    assert php.is_installed
+@pytest.mark.parametrize("name", [
+    "rh-php72",
+    "rh-php72-php-devel",
+    "rh-php72-php-fpm",
+    "rh-php72-php-gd",
+    "rh-php72-php-ldap",
+    "rh-php72-php-mbstring",
+    "rh-php72-php-opcache",
+    "rh-php72-php-pdo",
+    "rh-php72-php-pecl-apcu",
+    "rh-php72-php-xmlrpc",
+])
+def test_php_packages_installed(host, name):
+    pkg = host.package(name)
+    assert pkg.is_installed
